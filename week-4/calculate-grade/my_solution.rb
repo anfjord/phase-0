ecf24603a9641b4c4cfb79c70b90636a24af5e94
anfjord <lansfjord@gmail.com>
2016-03-07 04:25:20 -1000

    add minor changes to assignment

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -4,3 +4,32 @@
 
 
 # Your Solution Below
+
+def get_grade(average)
+
+
+	  case
+	    when average >= 90
+	      return "A"
+
+
+	    when average >= 80
+	      return "B"
+
+
+	    when average >= 70
+	      return "C"
+
+
+	    when average >= 60
+	      return "D"
+
+
+	    when average < 60
+	      return "F"
+
+
+	    else
+	      return "Not a valid average!"
+	  end
+	end
