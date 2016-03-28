536b51aae3c632589fe9ba58906cc92bcc468154
anfjord <lansfjord@gmail.com>
2016-03-27 20:37:32 -0700

    add minor changes

--- week-7/avascript_olympics.js
+++ week-7/avascript_olympics.js
@@ -11,23 +11,85 @@
 
 
 // Bulk Up
+var athlete1 = {
+  name: "Carl Lewis",
+  event: "Long-jump",
+}
+var athlete2 = {
+  name: "Roger Federer",
+  event: "Mens Singles Tennis",
+}
 
+var athleteArray = [athlete1, athlete2];
+
+function addWin(athleteArray){
+  for (var i = 0; i < athleteArray.length; i ++){
+    athleteArray[i].win = athleteArray[i].name + " has won " + athleteArray[i].event;
+  }
+  return athleteArray;
+}
+
+console.log(addWin(athleteArray));
+console.log(athlete1)
+console.log(athlete2)
 
 
 
 // Jumble your words
+function reverseString(string) {
 
+var array = string.split("");
+
+return array.reverse().join("");
+
+}
+
+console.log(reverseString("DevBootCamp"))
 
 
 
 // 2,4,6,8
 
+function evens (numbers){
+  var even_nums = []
+  for (var i in numbers) {
+    if (numbers[i]%2 == 0)
+      even_nums.push(numbers[i]);
+
+  }
+  return even_nums;
+}
+
+console.log(evens([0,1,2,3,4,5,6,7,8]));
 
 
 
 // "We built this city"
 
+function Athlete(name,age,sport,quote)
+{this.name = name;
+ this.age = age;
+ this.sport = sport;
+ this.quote = quote;
+  }
+
+
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 
 
 // Reflection
+
+// What JavaScript knowledge did you solidify in this challenge?
+//  -  How to iterate through an object and the constructor function.
+// What are constructor functions?
+// -  Logically similar to classes in Ruby.
+//    With constructors function you create objects and can specify the
+//    properties of an object and then pass in arguments.
+// How are constructors different from Ruby classes (in your research)?
+// -  different syntax, different set up where they are called,
+//    JS uses prototypes instead of classes.
+//
--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,25 +1,53 @@
-/ Separate Numbers with Commas in JavaScript **Pairing Challenge**
+//Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
 // I worked on this challenge with: .
 
 // Pseudocode
-
-
+// Write a function  that takes a variable with an integer as an argument
+// Convert integer to string, reverse and then split each number
+// Make a counter that place a comma on every three indexes.
+// Reverse and join into one string.
+// Print it
 
 // Initial Solution
 
 
+var separateComma = function(num){
+  var num_string = num.toString();
+  var num_split = num_string.split("");
+  var num_reverse = num_split.reverse();
+  for (var i = 3; i < num_reverse.length; i += 3){
+    num_reverse[i] += ",";
+  };
+  num_reverse.reverse();
+  var result = num_reverse.join("");
+  console.log(result);
+};
+
+separateComma(12345678);
 
 
 // Refactored Solution
 
+function separateComma(num){
+  var num_string = num.toString().split("").reverse();
+  for (var i = 3; i < num_string.length; i += 3){
+    num_string[i] += ",";
+  };
+  var result = num_string.reverse().join("");
+  console.log(result);
+};
 
-
-
-// Your Own Tests (OPTIONAL)
-
-
-
+separateComma(12345678);
 
 // Reflection
+
+//What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+// -
+//What did you learn about iterating over arrays in JavaScript?
+// -
+//What was different about solving this problem in JavaScript?
+// -
+//What built-in methods did you find to incorporate in your refactored solution?
+// -
